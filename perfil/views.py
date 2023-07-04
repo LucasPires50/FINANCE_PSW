dae20d402996ea3849c7b527c3f743557bc155ef
LucasPires50 <luc_50@outlook.com>
2023-07-04 15:57:14 -0300

    feat: configurações inicias para o cadastro de conta

--- perfil/views.py
+++ perfil/views.py
@@ -6,3 +6,15 @@ def home(request):
 
 def gerenciar(request):
     return render(request, 'gerenciar.html')
+
+def cadastrar_banco(request):
+    # Sempre que for para pegar dados normais é atraves do post
+    apelido = request.POST.get('apelido', None)
+    banco = request.POST.get('banco', None)
+    tipo = request.POST.get('tipo', None)
+    valor = request.POST.get('valor', None)
+    apelido = request.POST.get('apelido', None)
+    # Para pegar arquivo tem que usar o files
+    icone = request.FILES.get('icone', None)
+    
+    return render(request, 'cadastrar_banco.html')
