e8970c99040b17cd71d5e8d7d90c8497fe57ac17
LucasPires50 <luc_50@outlook.com>
2023-07-05 11:50:41 -0300

    feat: view extrato

--- extrato/views.py
+++ extrato/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 from django.contrib.messages import constants
 from extrato.utils import calcular_operação
 
+from datetime import datetime, timedelta
+
 from perfil.models import Conta, Categoria
 from .models import Valores
 
@@ -36,3 +38,23 @@ def novo_valor(request):
     # get_tipo_display(), serve para acessar o valor do choice
     messages.add_message(request, constants.SUCCESS, f'{valores.get_tipo_display()} registrada com sucesso!')
     return redirect('/extrato/novo_valor/')
+
+def view_extrato(request):
+    contas = Conta.objects.all()
+    categorias = Categoria.objects.all()
+    valores = Valores.objects.filter(data__month=datetime.now().month)
+    conta_get = request.GET.get('conta', None)
+    categoria_get = request.GET.get('categoria', None)
+    periodo_get = request.GET.get('periodo', None)
+    
+    if conta_get:
+        valores = valores.filter(conta_id=conta_get)
+    if categoria_get:
+        valores = valores.filter(categoria_id=categoria_get)
+    if periodo_get:
+            ultimos_sete_dias = datetime.now().date() - timedelta(days=7)
+            valores = valores.filter(data__range=(ultimos_sete_dias, datetime.now().date()))
+    
+    
+    #TODO: Fazer botão para limpar os filtros
+    return render(request, 'view_extrato.html', {"valores":valores, "categorias":categorias, "contas":contas})
