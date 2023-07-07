646059ddbb02fad0fe8e52ab0a38f4c27c8d9273
LucasPires50 <luc_50@outlook.com>
2023-07-07 11:20:41 -0300

    feat: updade categoria

--- planejamento/views.py
+++ planejamento/views.py
@@ -1,6 +1,9 @@
 import json
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib import messages
+from django.contrib.messages import constants
 
 from perfil.models import Categoria
 
@@ -11,8 +14,8 @@ def definir_planejamento(request):
 @csrf_exempt
 def update_valor_categoria(request, id):
     categoria = Categoria.objects.get(id=id)
-    novo_valor = json.loads(request).get('novo_valor', None)
+    novo_valor = json.load(request).get('novo_valor', None)
     if novo_valor:
-        categoria.valor = novo_valor
+        categoria.valor_planejamento = novo_valor
         categoria.save()
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    return JsonResponse({'status': 'success'})
