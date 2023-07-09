ab06a7980431246bf5b8132bf36c198231458868
LucasPires50 <luc_50@outlook.com>
2023-07-09 12:51:17 -0300

    feat: remover codigos repetidos

--- contas/views.py
+++ contas/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.messages import constants
 
-from datetime import datetime
+from contas.utils import bucar_contas
 
 from perfil.models import Categoria
-from .models import ContaPagar, ContaPaga
+from .models import ContaPagar
 
 def definir_contas(request):
     if request.method == 'GET':
@@ -30,15 +30,7 @@ def definir_contas(request):
             return redirect(r'/contas/definir_contas')
 
 def ver_contas(request):
-    MES_ATUAL = datetime.now().month
-    DIA_ATUAL = datetime.now().day
-    
-    contas = ContaPagar.objects.all()
-    contas_pagas = ContaPaga.objects.filter(data_pagamento__month=MES_ATUAL).values('conta')
-    contas_vencidas = contas.filter(dia_pagamento__lt=DIA_ATUAL).exclude(id__in=(contas_pagas))
-    contas_proximas_vencimento = contas.filter(dia_pagamento__lte=DIA_ATUAL+5).filter(dia_pagamento__gt=DIA_ATUAL).exclude(id__in=(contas_pagas))
-    restantes = contas.exclude(id__in=(contas_vencidas)).exclude(id__in=(contas_proximas_vencimento)).exclude(id__in=(contas_pagas))
-    
+    contas_pagas, contas_vencidas, contas_proximas_vencimento, restantes = bucar_contas()
     
     return  render(request, 'ver_contas.html', {
                                             'contas_pagas': contas_pagas, 
